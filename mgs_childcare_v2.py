84549aa82adc25de60237d1edec9781fd3ac1e8e
jeongminkimm <kimj7@middleton.school.nz>
2025-02-14 09:21:00 +1300

    mgs_childcare

---
+++ mgs_childcare_v2.py
@@ -0,0 +1,21 @@
+"""Program for a child day-care centre to keep track of children throughout
+the day - v1
+Asking the user's choice
+Created By Jeongmin Kim
+"""
+
+
+# Main routine
+print("************************************************************")
+print("Welcome to MGS Childcare")
+print("What would you like to do? Please choose one of the items below")
+print("************************************************************")
+print()
+print("1. Drop off a child")
+print("2. Pick up a child")
+print("3. Calculate cost")
+print("4. Print roll")
+print("5. Exit the system")
+print()
+choice = int(input("Enter your choice (number between 1 and 5): "))
+print()
